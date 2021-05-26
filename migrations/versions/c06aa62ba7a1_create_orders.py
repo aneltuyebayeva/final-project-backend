30ddacd7fc156d94858f05299be1c85f6989cc35
Anel Tuyebayeva <aneltuyebayeva@Anels-MacBook-Pro.local>
2021-05-26 15:55:08 -0400

    changes in migrations

--- migrations/versions/c06aa62ba7a1_create_orders.py
+++ migrations/versions/c06aa62ba7a1_create_orders.py
@@ -11,7 +11,7 @@ import sqlalchemy as sa
 
 # revision identifiers, used by Alembic.
 revision = 'c06aa62ba7a1'
-down_revision = 'ae6230cbe2c8'
+down_revision = '608edca83238'
 branch_labels = None
 depends_on = None
 
